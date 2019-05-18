326224dda190ce3025986762243d6ab3b6e70196
friedpies <kenmarut@gmail.com>
2019-05-18 11:27:49 -0500

    trying to add mongo

--- server/index.js
+++ server/index.js
@@ -1,24 +1,24 @@
 const express = require("express");
 const app = express();
-// const { Shoe } = require("../database/index.js");
+const { Shoe } = require("../database/index.js");
 const bodyParser = require("body-parser");
 const port = 3000;
 
 app.use(express.static(__dirname + "/../client/dist"));
 
-// app.get("/api/:info/:sku", (req, res) => {
-//   const sku = req.params.sku;
-//   const info = req.params.info;
-//   console.log(description);
-//   Shoe.findOne({ sku })
-//     .exec((err, shoe) => {
-//       res.json(shoe[info]);
-//     })
-//     .catch(err => {
-//       console.log("there was an error finding the item");
-//     });
-//   console.log(req.params.sku);
-// });
+app.get("/api/:info/:sku", (req, res) => {
+  const sku = req.params.sku;
+  const info = req.params.info;
+  console.log(description);
+  Shoe.findOne({ sku })
+    .exec((err, shoe) => {
+      res.json(shoe[info]);
+    })
+    .catch(err => {
+      console.log("there was an error finding the item");
+    });
+  console.log(req.params.sku);
+});
 
 app.listen(port, () => {
   console.log("Ken's service listening on port, ", port);

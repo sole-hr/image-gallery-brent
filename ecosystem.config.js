d6e11b2aa333e275ab6248c4bffa9989e0f98abc
friedpies <kenmarut@gmail.com>
2019-05-18 10:54:22 -0500

    modified ecosystem folder

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,7 @@ module.exports = {
     production: {
       user: 'ubuntu',
       host: 'ec2-18-225-10-188.us-east-2.compute.amazonaws.com',
-      key: '~/.ssh/image-gallery.pem',
+      key: '~/.ssh/image-gallery-deploy.pem',
       ref: 'origin/master',
       repo: 'git@github.com:nike-hratx41-fec/image-gallery.git',
       path: '/home/ubuntu/image-gallery',

c76b8f95b8f9b2e00cdcf1efd6330c685123a2ef
friedpies <kenmarut@gmail.com>
2019-05-18 12:19:44 -0500

    testing deployment

--- server/index.js
+++ server/index.js
@@ -10,7 +10,6 @@ app.use(express.static(__dirname + "/../client/dist"));
 app.get("/api/:info/:sku", (req, res) => {
   const sku = req.params.sku;
   const info = req.params.info;
-  console.log(description);
   Shoe.findOne({ sku })
     .exec((err, shoe) => {
       res.json(shoe[info]);

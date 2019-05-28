824f9929a9dff432bf377efe3fef2173356b1f83
friedpies <kenmarut@gmail.com>
2019-05-28 11:27:57 -0500

    successfully deployed without db

--- server/index.js
+++ server/index.js
@@ -3,9 +3,9 @@ console.log("ENV VARIABLES ", process.env.PORT);
 const express = require("express");
 const cors = require('cors');
 const app = express();
-// const {
-//   Shoe
-// } = require("../database/index.js");
+const {
+  Shoe
+} = require("../database/index.js");
 const port = process.env.PORT || 3001;
 const bodyParser = require("body-parser");
 
@@ -13,24 +13,24 @@ app.use(cors());
 app.use(express.static(__dirname + "/../client/dist"));
 
 app.get("/api/:info/:sku", (req, res) => {
-  // const sku = req.params.sku;
-  // const info = req.params.info;
-  // Shoe.findOne({
-  //     sku
-  //   })
-  //   .exec((err, shoe) => {
-  //     let responseData = {};
-  //     if (info === "title") {
-  //       responseData = {
-  //         productName: shoe.productName,
-  //         price: shoe.price,
-  //         category: shoe.category
-  //       };
-  //     } else {
-  //       responseData = shoe[info];
-  //     }
-  //     res.json(responseData);
-  //   })
+  const sku = req.params.sku;
+  const info = req.params.info;
+  Shoe.findOne({
+      sku
+    })
+    .exec((err, shoe) => {
+      let responseData = {};
+      if (info === "title") {
+        responseData = {
+          productName: shoe.productName,
+          price: shoe.price,
+          category: shoe.category
+        };
+      } else {
+        responseData = shoe[info];
+      }
+      res.json(responseData);
+    })
 }); 
 
 app.listen(port, () => {

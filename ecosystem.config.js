8037f72bc03d46bf10fb4ac5ba9e3616522fb167
friedpies <kenmarut@gmail.com>
2019-05-28 11:06:28 -0500

    unignoring config file

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'image-gallery',
+    script: './server/index.js',
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-18-225-10-188.us-east-2.compute.amazonaws.com',
+      key: '~/.ssh/image-gallery-deploy.pem',
+      ref: 'origin/master',
+      repo: 'git@github.com:nike-hratx41-fec/image-gallery.git',
+      path: '/home/ubuntu/image-gallery',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+}

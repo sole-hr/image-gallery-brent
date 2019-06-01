1d4eef06656d3c34c2f50bedd6d8f8f0e9b539e0
friedpies <kenmarut@gmail.com>
2019-06-01 14:04:55 -0500

    redeploy imagegallery

--- client/src/components/CartFavoriteButtons.jsx
+++ client/src/components/CartFavoriteButtons.jsx
@@ -67,11 +67,11 @@ class CartFavoriteButtons extends React.Component {
       }
     });
     window.dispatchEvent(onCartClick);
-    window.alert(
-      `Product ID: ${this.state.sku}\nColor: ${this.state.color}\nSize: ${
-        this.state.size
-      }`
-    );
+    // window.alert(
+    //   `Product ID: ${this.state.sku}\nColor: ${this.state.color}\nSize: ${
+    //     this.state.size
+    //   }`
+    // );
   }
 
   render() {
